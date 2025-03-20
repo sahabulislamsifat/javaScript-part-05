b3d23e0fbffd7c69a795bd1b3b2d6dd01244a2a7
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-20 16:33:57 +0600

    js type conversion

---
+++ typeConversion.js
@@ -0,0 +1,20 @@
+// console.log(Number("   "));
+
+// console.log(parseFloat("3.2"));
+// console.log(parseInt("3.2"));
+// console.log(parseInt("3.6"));
+
+// convert string
+// console.log(String(100 + 23));
+// console.log(typeof String(100 + 23));
+
+// let a = 10e6;
+
+// console.log(a.toExponential());
+
+// let a = 10.54;
+// let a = 10.5354;
+let a = 10.5654;
+// console.log(a.toFixed());
+
+console.log(a.toPrecision(3));
