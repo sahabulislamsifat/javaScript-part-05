be60fe741743b1e2d1c1241104befb99918b12f2
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-03-20 21:21:29 +0600

    temporary closed

---
+++ scope.js
@@ -0,0 +1,42 @@
+// if (x) {
+//     var x
+// }
+
+// function a() {
+//     let x = 4
+// }
+
+// function b() {
+//     let x = 10
+// }
+
+// var carName = "Honda";
+
+// console.log(carName);
+// console.log(window.carName);
+
+// let str = "hello";
+
+// if (0 > 5) {
+//   str = "petter";
+// }
+
+// console.log(str);
+
+// console.log(str);
+
+// let str = "hello world!";
+
+let babyAge = 1;
+let isBirthday = true;
+
+if (isBirthday) {
+  let babyAge = 2;
+}
+
+console.log(babyAge);
+
+// {
+//   console.log(typeof name);
+//   let name = "sifat";
+// }
